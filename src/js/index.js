bdb1d9774f31a089c68d466a86b2405f6821e7ad
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-29 23:10:20 -0300

    comecando a logica do site

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-window.alert('hello, world!')
+/*objetivo 1 - quando clicarmos na seta de avancas temos que mostrar o róximo cartao da lista 
+    passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+    passo 2 - dar um jeiro de identidicar o click do usuario na seta avancar 
+    passo 3 - fazer aparecer o proximo cartao da lista 
+    passo 4 - buscar o cartao que esta selecionado e esconder 
+
+
+    objetivo 1 - quando clicarmos na seta de voltar temos que mostrar o card anterior da lista 
+    passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+    passo 2 - dar um jeiro de identidicar o click do usuario na seta voltar 
+    passo 3 - fazer aparecer o  cartao anterior da lista 
+    passo 4 - buscar o cartao que esta selecionado e esconder */
+    
+
+
+    
+
+
+
+
+  
